772ad7cecee6453b0b126fb905a878ede0d19cfb
MGalaxy1984 <zhxie@ucsd.edu>
2021-08-24 22:01:17 -0700

    preprocessing #1

--- debug.py
+++ debug.py
@@ -1,6 +1,8 @@
 import glob
 import os
 
+import pretty_midi
+
 
 def file_number(path, extension='*'):
     return len(glob.glob(path + '/' + '*.' + extension))
@@ -22,6 +24,24 @@ def file_keep_extension(path, extension=None):
             os.remove(os.path.join(path, item))
 
 
+def remove_single_track_samples(path, keyword):
+    for item in os.listdir(path):
+        if keyword in item:
+            print('Removing:', item)
+            os.remove(os.path.join(path, item))
+
+
+def print_notes_sequence(file_name_with_path):
+    midi_data = pretty_midi.PrettyMIDI(file_name_with_path)
+    print('Number of notes:', len(midi_data.instruments[0].notes))
+    for instrument in midi_data.instruments:
+        for note in instrument.notes:
+            duration = note.end - note.start
+            print(note.pitch, 'Duration = ', duration)
+
+
 def test():
     print(file_number('piano'))
+    remove_single_track_samples('piano', 'format0')
     # file_keep_extension(path='piano', extension=['mid', 'midi'])
+    print(file_number('piano'))
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import music21
 
 import debug
+import preprocess
 
-debug.test()
+# debug.test()
+# debug.print_notes_sequence('test_output/piano/lizet_et6_track2.mid')
+
+preprocess.test()
+# preprocess.process_piano()
